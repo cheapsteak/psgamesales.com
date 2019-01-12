791dc7d4abfe17f84c6d88a60a755a7db59a3888
Chang Wang <garnwraly@gmail.com>
2019-01-12 15:30:01 -0500

    make available build commit hash

---
+++ src/constants/git.ts
@@ -0,0 +1,1 @@
+export const GIT_HASH = process.env.REACT_APP_COMMIT_HASH;
--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { GIT_HASH } from './constants/git';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+window['UI_COMMIT_HASH'] = GIT_HASH;
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
