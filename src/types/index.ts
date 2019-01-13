085a74705f3020da65df85336f6d2d2e7f32d244
Chang Wang <garnwraly@gmail.com>
2019-01-13 13:53:03 -0500

    Add comments for how platform and gametypes can be used in api response

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,11 +1,15 @@
 export * from './apiResponseTypes';
 
+// these affect the data.attributes['total-results'] of the server response
+
+// ?platform=ps4
 export enum Platform {
   PS4 = 'ps4',
   PS3 = 'ps3',
   Vita = 'vita',
 }
 
+// ?game_type=ps4_full_games
 export enum GameType {
   PS4_Full_Games = 'ps4_full_games',
   PS3_Full_Games = 'ps3_full_games',
