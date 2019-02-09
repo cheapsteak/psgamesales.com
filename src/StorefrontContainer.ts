4d3ba581363e793f9aa6d22ceeaa280e504ab604
Chang Wang <garnwraly@gmail.com>
2019-02-09 15:16:18 -0500

    delete useStorefront, make context only way to consume storefront list

--- src/StorefrontContainer.ts
+++ src/StorefrontContainer.ts
@@ -1,6 +1,30 @@
+import { useContext, useState, useEffect } from 'react';
 import createContainer from 'constate';
+import { UserOptionsContext } from 'src/UserOptionsContext';
+import { StorefrontItem } from 'src/types/StoreMetaData';
+import fetchStorefronts from 'src/requests/fetchStorefronts';
 
-import useStorefronts from './useStorefronts';
+const useStorefronts = () => {
+  const { country } = useContext(UserOptionsContext);
+  const [storefronts, setStorefronts] = useState([] as StorefrontItem[]);
+
+  useEffect(
+    () => {
+      if (!country) {
+        return;
+      }
+      fetchStorefronts(country.code).then(fetchedStorefronts => {
+        if (!fetchedStorefronts || !fetchedStorefronts.length) {
+          throw new Error('No storefronts could be found');
+        }
+        setStorefronts(fetchedStorefronts);
+      });
+    },
+    [country && country.key],
+  );
+
+  return storefronts;
+};
 
 const StorefrontContainer = createContainer(useStorefronts);
 
