6ad23c15f1ae246e10557a4fe45b981ce5f05486
Chang Wang <garnwraly@gmail.com>
2019-02-09 17:27:05 -0500

    have storefront accept classname

--- src/Controls/StorefrontList.tsx
+++ src/Controls/StorefrontList.tsx
@@ -1,12 +1,14 @@
-import React, { useContext } from 'react';
+import React, { useContext, HtmlHTMLAttributes } from 'react';
 import { Link } from '@reach/router';
 import { css, cx } from 'emotion';
 import StorefrontContainer from '../StorefrontContainer';
 
-const StorefrontList: React.FunctionComponent<{}> = () => {
+const StorefrontList: React.FunctionComponent<
+  HtmlHTMLAttributes<HTMLDivElement>
+> = ({ className, ...props }) => {
   const storefronts = useContext(StorefrontContainer.Context);
   return (
-    <div className="FacetWrapper">
+    <div className={cx('FacetWrapper', className)} {...props}>
       <h2>Stores</h2>
 
       {storefronts.map(storefront => (
