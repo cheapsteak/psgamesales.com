9552cc84a583c9278fdea25f75073c6b74fbf23a
Chang Wang <garnwraly@gmail.com>
2019-02-11 18:12:54 -0500

    use service-worker

--- src/index.tsx
+++ src/index.tsx
@@ -12,4 +12,4 @@ window['UI_COMMIT_HASH'] = GIT_HASH;
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
