dbd58ff9d52cc968afc83ff04f5393979bbc1595
Chang Wang <garnwraly@gmail.com>
2019-02-12 19:32:57 -0500

    Add file for regions storefronts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,5 @@
 export { default as colors } from './colors';
 export { default as countries } from './countries';
 export { default as facets } from './facets';
-export { default as DEFAULT_STOREFRONTS } from './DEFAULT_STOREFRONTS';
+export * from './storefronts';
 export * from './css';
---
+++ src/constants/storefronts.ts
@@ -0,0 +1,85 @@
+import { StorefrontItem } from '../types/StoreMetaData';
+
+export interface RegionStorefronts {
+  GAMES_NAV: string;
+  DEALS_SUB_NAV?: string;
+
+  ALL_DEALS: StorefrontItem;
+  PSPLUS_FREE_GAMES: StorefrontItem;
+  PSPLUS_DISCOUNTS: StorefrontItem;
+  PSPLUS_EXCLUSIVES: StorefrontItem;
+}
+
+export const AMERICAN_STOREFRONTS: RegionStorefronts = {
+  GAMES_NAV: 'STORE-MSF77008-GAMESHOMEPAGE',
+  DEALS_SUB_NAV: 'STORE-MSF77008-SAVE',
+
+  ALL_DEALS: {
+    id: 'STORE-MSF77008-ALLDEALS',
+    name: 'All Deals',
+  },
+
+  PSPLUS_FREE_GAMES: {
+    id: 'STORE-MSF77008-PSPLUSFREEGAMES',
+    name: 'PS+ Monthly Games',
+  },
+
+  PSPLUS_DISCOUNTS: {
+    id: 'STORE-MSF77008-PSPLUSDISCOUNTS',
+    name: 'PS+ Discounts',
+  },
+
+  PSPLUS_EXCLUSIVES: {
+    id: 'STORE-MSF77008-PSPLUSEXCLUSIVES',
+    name: 'PS+ Exclusives',
+  },
+};
+
+export const EUROPEAN_STOREFRONTS: RegionStorefronts = {
+  GAMES_NAV: 'STORE-MSF75508-GAMESCENE',
+  DEALS_SUB_NAV: 'STORE-MSF75508-GAMESPECIALOFF',
+
+  ALL_DEALS: {
+    id: 'STORE-MSF75508-PRICEDROPSCHI',
+    name: 'Price Drops',
+  },
+
+  PSPLUS_FREE_GAMES: {
+    id: 'STORE-MSF75508-PLUSINSTANTGAME',
+    name: 'PS+ Monthly Games',
+  },
+
+  PSPLUS_DISCOUNTS: {
+    id: 'STORE-MSF75508-MEMBERSV2',
+    name: 'PS+ Discounts',
+  },
+
+  PSPLUS_EXCLUSIVES: {
+    id: 'STORE-MSF75508-PLUSEXCLUSIVES',
+    name: 'PS+ Exclusives',
+  },
+};
+
+export const ASIAN_STOREFRONTS: RegionStorefronts = {
+  GAMES_NAV: 'STORE-MSF86012-GAMEHOMEPAGE',
+
+  ALL_DEALS: {
+    id: 'STORE-MSF86012-SPECIALOFFER',
+    name: 'Special Offers',
+  },
+
+  PSPLUS_FREE_GAMES: {
+    id: 'STORE-MSF86012-PLUS_FTT_CONTENT',
+    name: 'PS+ Free Games',
+  },
+
+  PSPLUS_DISCOUNTS: {
+    id: 'STORE-MSF86012-PLUS_DIS_CONTENT',
+    name: 'PS+ Discounts',
+  },
+
+  PSPLUS_EXCLUSIVES: {
+    id: 'STORE-MSF86012-PLUS_EXCLUS_CONT',
+    name: 'PS+ Exclusives',
+  },
+};
