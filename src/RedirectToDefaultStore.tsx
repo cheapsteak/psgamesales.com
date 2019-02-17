dc68e0120c7e59fe82585a80b56257f0fc62030b
Chang Wang <garnwraly@gmail.com>
2019-02-17 16:20:48 -0500

    Redirect / to first storefront in list
    "All Deals" doesn't actually contain all deals
    Storefronts in the list are more likely to contain interesting sales

--- src/RedirectToDefaultStore.tsx
+++ src/RedirectToDefaultStore.tsx
@@ -1,21 +1,16 @@
 import React, { useContext } from 'react';
 import { Redirect } from '@reach/router';
-import { getRegionStorefrontsByCountryCode } from 'src/utils';
-import { UserOptionsContext } from 'src/UserOptionsContext';
+import StorefrontContainer from 'src/StorefrontContainer';
 
 const RedirectToDefaultStore: React.FunctionComponent = () => {
-  const { country: countryFromUserOptions } = useContext(UserOptionsContext);
+  const storefronts = useContext(StorefrontContainer.Context);
 
-  if (!countryFromUserOptions) {
-    // wait a bit for country to return;
+  if (storefronts.length === 0) {
+    // wait a bit for storefronts to return
     return null;
   }
 
-  const regionStorefronts = getRegionStorefrontsByCountryCode(
-    countryFromUserOptions.code,
-  );
-
-  return <Redirect noThrow to={`stores/${regionStorefronts.ALL_DEALS.id}`} />;
+  return <Redirect noThrow to={`stores/${storefronts[0].id}`} />;
 };
 
 export default RedirectToDefaultStore;
